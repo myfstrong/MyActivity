aa8b3a985ee6b45425a33d178b7c1bfabc5ab28a
myfstrong <4821825@qq.com>
2016-03-02 17:06:53 +0800

    Initial commit

---
+++ app/src/main/java/com/myfstrong/secondactivity/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.myfstrong.secondactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.myfstrong.myactivity.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
---
+++ app/src/main/java/com/myfstrong/thirdactivity/ThirdActivity.java
@@ -0,0 +1,17 @@
+package com.myfstrong.thirdactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.myfstrong.myactivity.R;
+
+/**
+ * Created by Administrator on 2016/3/2 0002.
+ */
+public class ThirdActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
